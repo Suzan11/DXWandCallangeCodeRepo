021edcacb3509a0cb9d8669f920ed63eab9a3ee5
Suzan11 <eng.suzan.iti@gmail.com>
2022-07-23 19:13:56 +0200

    Create delete-stack.sh

---
+++ scripts/delete-stack.sh
@@ -0,0 +1,19 @@
+echo " In delete stack stage now ................."
+echo " Test is passed and we're going to clean the stack"
+
+  aws cloudformation validate-template --template-body file://DXwandstack.yml 
+  aws cloudformation  delete-stack \
+    --stack-name DXwandstack
+   echo " Sleeping 5 minutes untill stack is fully deleted..........."
+   sleep 5m
+  stack_status=$(aws cloudformation describe-stacks --stack-name DXwandstack 2>/dev/null)
+ if [ -z "$stack_status" ]
+ then
+   echo "Stack is deleted sucessfully ."
+else
+   echo "Seems there's an issue happened while deleting the stack, Please check the logs for mor info."
+
+fi
+
+Footer
+© 2022 GitHub, Inc.
